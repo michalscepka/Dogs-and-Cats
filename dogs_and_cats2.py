c2c6d208a2f487b10baaf14d64db4f0fcbdc13f7
michalscepka <scepkamichal1@gmail.com>
2020-03-15 10:41:44 +0100

    tensorboard bruteforce

--- dogs_and_cats2.py
+++ dogs_and_cats2.py
@@ -7,10 +7,6 @@ import pickle
 import numpy as np
 import time
 
-NAME = "Cats-vs-dog-cnn-64x2-{}".format(int(time.time()))
-
-tensorboard = TensorBoard(log_dir='logs\{}'.format(NAME))
-
 X = pickle.load(open("X.pickle","rb"))
 y = pickle.load(open("y.pickle","rb"))
 
@@ -18,25 +14,35 @@ y = pickle.load(open("y.pickle","rb"))
 X = tf.keras.utils.normalize(X, axis=1)
 y = np.array(y)
 
-model = Sequential()
-model.add(Conv2D(64, (3, 3), input_shape = X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}--conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            tensorboard = TensorBoard(log_dir='logs\{}'.format(NAME)) # tensorboard --logdir=logs  shows tenderboard
+            print(NAME)
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+            model = Sequential()
 
-model.add(Flatten())
+            model.add(Conv2D(layer_size, (3, 3), input_shape = X.shape[1:]))
+            model.add(Activation("relu"))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
 
-#model.add(Dense(64))
-#model.add(Activation("relu"))
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation("relu"))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+            model.add(Flatten()) # this converts our 3D feature maps to 1D feature vectors
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation("relu"))
 
-model.fit(X, y, batch_size=32, epochs=20, validation_split=0.1, callbacks=[tensorboard])
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            model.compile(loss='binary_crossentropy',
+                        optimizer='adam',
+                        metrics=['accuracy'])
+
+            model.fit(X, y, batch_size=32, epochs=20, validation_split=0.1, callbacks=[tensorboard])
