b85c019a5c92a556cd570d22e704ad6f844fb280
michalscepka <scepkamichal1@gmail.com>
2020-03-18 11:45:03 +0100

    Added function predict_images

--- 1.py
+++ 1.py
@@ -2,10 +2,10 @@
 #                           Loading in our own data
 #------------------------------------------------------------------------------
 
-import numpy as np  #various array operations
-import matplotlib.pyplot as plt     #show the image
-import os   #directories and paths in os
-import cv2  #image operations
+import numpy as np  # various array operations
+import matplotlib.pyplot as plt     # show the image
+import os   # directories and paths in os
+import cv2  # image operations
 import random
 import pickle
 
@@ -18,20 +18,18 @@ training_data = []
 def create_training_data():
     for category in CATEGORIES:  # do dogs and cats
 
-        path = os.path.join(DATADIR,category)  # create path to dogs and cats dir
+        path = os.path.join(DATADIR, category)  # create path to dogs and cats dir
         class_num = CATEGORIES.index(category)  # get the classification  (0 or a 1). 0=dog 1=cat
 
         for img in os.listdir(path):  # iterate over each image per dogs and cats
             try:
-                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
+                img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)  # convert to array
                 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
                 training_data.append([new_array, class_num])  # add this to our training_data
+                plt.imshow(img_array, cmap="gray")
+                plt.show()
             except Exception as e:  # in the interest in keeping the output clean...
                 pass
-            #except OSError as e:
-            #    print("OSErrroBad img most likely", e, os.path.join(path,img))
-            #except Exception as e:
-            #    print("general exception", e, os.path.join(path,img))
 
 create_training_data()
 print(len(training_data))
--- 5.py
+++ 5.py
@@ -4,8 +4,11 @@
 
 import cv2
 import tensorflow as tf
+import os   # directories and paths in os
 
 CATEGORIES = ["Dog","Cat"] # will use this to convert prediction num to string value
+DATADIR = "C:/Users/Michal/Dropbox/Neural network/dogs_and_cats/pictures"
+model = tf.keras.models.load_model("3x32x1-CNN.model")
 
 def prepare(filepath):
     IMG_SIZE = 100
@@ -13,8 +16,13 @@ def prepare(filepath):
     new_array = cv2.resize(img_array,(IMG_SIZE,IMG_SIZE)) # resize image to match model's expected sizing
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1) # return the image with shaping that TF wants
 
-model = tf.keras.models.load_model("64x3-CNN.model")
+def predict_images(datadir):
+    path = os.path.join(datadir)
+    for img in os.listdir(path):  # iterate over each image per dogs and cats
+        try:
+            prediction = model.predict([prepare(os.path.join(path, img))]) # always predict a list
+            print("Prediction for {}: ".format(img), CATEGORIES[int(prediction[0][0])])
+        except Exception as e:
+            pass
 
-prediction = model.predict([prepare('pictures/cat1.jpg')]) # always predict a list
-
-print("Prediction: ", CATEGORIES[int(prediction[0][0])])
+predict_images(DATADIR)
